70a571f34b5d3791b34f048c803a2b6ba8deb226
Nicolo Traini <nicolo.traini@oncode.it>
2021-12-06 13:51:35 +0100

    Add json persistence layer

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
 import Control.Lens
+import Data.Aeson
 import Data.Maybe
 import Data.Text (Text)
+import qualified Data.ByteString.Lazy as B
 import Monomer
 import TextShow
 
 import qualified Monomer.Lens as L
 import Data.String (IsString)
+import GHC.Generics
 
 data Tip = Tip
   { _ts :: Int
+  , _title :: Text
   , _content :: Text
-  , _visible :: Bool
-  } deriving (Eq, Show)
+  , _titleVisible :: Bool
+  , _contentVisible :: Bool
+  } deriving (Eq, Show, Generic)
+
+instance FromJSON Tip
+instance ToJSON Tip
 
 data AppModel = AppModel
   { _tips :: [Tip]
@@ -26,11 +35,11 @@ data AppModel = AppModel
   } deriving (Eq, Show)
 
 data AppEvent
-  = AppInit
+  = LoadTips
+  | SetTips [Tip]
   | AddTip
   | SearchTip
   | RemoveTip Int
-  | AppIncrease
   deriving (Eq, Show)
 
 makeLenses 'Tip
@@ -57,14 +66,17 @@ buildUI wenv model = widgetTree where
 
     , separatorLine `styleBasic` [paddingT 20]
 
-    , vstack (zipWith listTips [0..] (filter (^.visible) (model^.tips)))
+    , vstack (zipWith listTips [0..] (model^.tips))
     ] `styleBasic` [padding 10]
 
   titleText text = label text `styleBasic` [textFont "Medium", textSize 20]
   listTips id tip = vstack
-    [ label_ (tip^.content) [ellipsis] `styleBasic` [paddingH 10]
+    [ label_ (tip^.title) [ellipsis] `styleBasic` [paddingH 10] `nodeVisible` tip^.titleVisible
+    , label_ (tip^.content) [ellipsis] `styleBasic` [paddingH 10] `nodeVisible` tip^.contentVisible
     ]
-      `nodeKey` showt (tip^.ts) `styleBasic` [paddingT 10]
+      `nodeKey` showt (tip^.ts)
+
+      `styleBasic` [paddingT 10]
 
 handleEvent
   :: WidgetEnv AppModel AppEvent
@@ -73,29 +85,41 @@ handleEvent
   -> AppEvent
   -> [AppEventResponse AppModel AppEvent]
 handleEvent wenv node model evt = case evt of
-  AppInit -> []
+  LoadTips -> [ Task $ SetTips . fromJust . decode <$> B.readFile "tips-list.json"
+              ]
+  -- show all tips and reset search box
+  SetTips ts -> [ Model $ model
+                    & searchBoxText .~ ""
+                    & tips .~ ts
+                    & (tips . traverse . titleVisible) %~ const True
+                , SetFocusOnKey tipSearchBoxKey
+                ]
   SearchTip ->
     [ Model $ case model^.searchBoxText of
-        -- TODO find a better way to do this...
         -- set all tips visible
-        "" -> model & (tips . traverse . visible) %~ const True
+        "" -> model & (tips . traverse . titleVisible) %~ const True
         -- only match tips with same content as text-field
-        _  -> model & tips %~ map (\x -> x & visible .~ ((model^.searchBoxText) == (x^.content)))
+        _  -> model & tips %~ map (\x -> x & titleVisible .~ ((model^.searchBoxText) == (x^.title)))
     ]
   AddTip | model^.searchBoxText /= "" ->
-    [ Model $ model
-        & searchBoxText .~ ""
-        -- add new tip to list
-        & tips %~ (newTip :)
-        -- set all tips visible
-        & (tips . traverse . visible) %~ const True
-    , SetFocusOnKey tipSearchBoxKey
+    [ Task $ SetTips <$> do
+        let newTips = newTip : (model^.tips)
+        B.writeFile "tips-list.json" $ encode newTips
+        return newTips
     ]
+  -- AddTip | model^.searchBoxText /= "" ->
+  --   [ Model $ model
+  --       & searchBoxText .~ ""
+  --       -- add new tip to list
+  --       & tips %~ (newTip :)
+  --       -- set all tips visible
+  --       & (tips . traverse . titleVisible) %~ const True
+  --   , SetFocusOnKey tipSearchBoxKey
+  --   ]
   RemoveTip id -> []
-  AppIncrease -> [Model (model & clickCount +~ 1)]
   _ -> []
   where
-    newTip = Tip (wenv ^. L.timestamp) (model^.searchBoxText) True
+    newTip = Tip (wenv ^. L.timestamp) (model^.searchBoxText) "dummycontent" True False
 
 tipSearchBoxKey :: (IsString a) => a
 tipSearchBoxKey = "tipSearchBoxKey"
@@ -111,10 +135,10 @@ main = do
       appFontDef "Medium" "./assets/fonts/Roboto-Medium.ttf",
       appFontDef "Bold" "./assets/fonts/Roboto-Bold.ttf",
       appFontDef "Italic" "./assets/fonts/Roboto-Italic.ttf",
-      appInitEvent AppInit
+      appInitEvent LoadTips
       ]
     model = AppModel
-      { _tips = [Tip 1 "tippo" True, Tip 2 "tappo" True]
+      { _tips = []
       , _searchPromptText = "Search tip"
       , _searchBoxText = ""
       , _clickCount = 0
